d876c7f72c47ad2391200144c5a6300c45e2afbd
01eksandr0 <krux228@gmail.com>
2024-05-12 14:34:53 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -20,15 +20,12 @@ const getTrendMovies = async () => {
 
 const getMoviesByWord = async (query) => {
   try {
-    const response = await apiInstance.get(
-      `search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDk3YWNmY2JiMDUzYjBlODcwMmZhZDY1Mzc0OTM4ZiIsInN1YiI6IjY1ZDBhZjZjMWQzMTQzMDE4NGJhMWIxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-BvX4OF2A1Bvvo_nFUQyFPBFoWdgy3JZZJ61-IhEeLc",
-        },
-      }
-    );
+    const response = await apiInstance.get(`search/movie?query=${query}`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDk3YWNmY2JiMDUzYjBlODcwMmZhZDY1Mzc0OTM4ZiIsInN1YiI6IjY1ZDBhZjZjMWQzMTQzMDE4NGJhMWIxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-BvX4OF2A1Bvvo_nFUQyFPBFoWdgy3JZZJ61-IhEeLc",
+      },
+    });
     return response;
   } catch (error) {
     console.log(error);
